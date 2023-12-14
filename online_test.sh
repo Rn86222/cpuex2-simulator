d15d69dd3c025d2470b25b3f34f7e13737a00d78
Rn86222 <rn86222.light0213@gmail.com>
2023-12-14 22:05:44 +0900

    add online test

---
+++ online_test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+trap 'func' 1 2 3 15
+
+minrt='minrt_mini'
+
+function error() {
+    status=$?
+    cd ..
+    echo "Exit status: $status"
+    exit $status
+}
+
+trap 'error' 1 2 3 15
+rm ./$minrt.bin ./$minrt.data ./$minrt.ppm > /dev/null 2>&1
+
+echo -n "Compiling '$minrt.ml'... "
+cd cpuex-2-2023
+git checkout rn > /dev/null 2>&1
+./to_riscv
+make > /dev/null 2>&1
+rm test/$minrt.s > /dev/null 2>&1
+./min-caml test/$minrt > /dev/null 2>&1
+mv test/$minrt.s ../$minrt.s
+cd ..
+echo "done."
+
+echo -n "Assembling '$minrt.s'... "
+cd cpuex2-assembler
+rm ./$minrt.s ./$minrt.bin ./$minrt.data > /dev/null 2>&1
+mv ../$minrt.s ./$minrt.s
+cargo run --release -- --file $minrt.s --style bin > /dev/null 2>&1
+mv ./$minrt.bin ../$minrt.bin
+mv ./$minrt.data ../$minrt.data
+cd ..
+echo "done."
+
+echo -n "Simulating '$minrt.bin'... "
+ulimit -s unlimited && cargo run --release -- --bin $minrt.bin > /dev/null 2>&1
+echo "done."
+
+echo "Checking '$minrt.ppm'..."
+diff $minrt.ppm $minrt'_ans.ppm' > diff.txt
+if [ -s diff.txt ]; then
+    echo "Failed"
+else
+    echo "Success"
+fi
